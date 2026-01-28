60a4bef63510943bf234787fe2e46a75708bd4cd
TasneemCode <tasneem.ismail2003@gmail.com>
2026-01-28 14:32:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 27 17:32:56 2026
-
-@author: BBarsch
-"""
-
 import streamlit as st
+import time
 
-st.write("Hello2")
 
-st.title("Title heading")
+t1, t2, t3 = st.tabs(["Introduction", "Research", "Contact"])
 
-st.write("Hello, Streamlit!")
+with t1:
+    with st.empty():
+        st.title("Hello, I'm Tasneem Ismail!")
+        time.sleep(4)
+        st.header("Welcome to my page")
+        time.sleep(4)
+        st.title('''A bit about me...\n 
+        I completed my undergrad and Honours at UCT.
+        Undergrad: BSc in Computer Science and Genetics
+        Honours: Molecular & Cell Biology 
+        The simplest way to describe my research interest: Computational Drug Discovery & Design. 
+        Would you like to hear about my Honours research project?''')
+        st.feedback("thumbs")
 
-st.header("Number selection")
+with t2:
+    st.title("My Research")
+    st.header("Background")
+    st.write("Candida auris, network analysis, Mutation-Minimisation (MuMi)")
+    st.header("Workflow")
+    st.write("WT processing & minimization --> Mutant generation & Minimization --> Analysis")
+    st.header("Results")
+    st.write("Top effective residues and pathways")
 
-number = st.slider("Pick a number", 1, 100)
-st.write(f"You picked: {number}")
+with t3:
+    st.title('''Have any questions?
+    Contact me at tasneem.ismail2003@gmail.com
+             ''')
